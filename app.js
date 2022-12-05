566b8f56336c9dadab71db1171d068a5cff00f28
Musa Doci <docimusa@gmail.com>
2022-12-05 23:26:03 +0100

    adding components

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 function nvvv() {
-  return reactPage1;
+   return reactPage1, reactPage2;
 }
 const reactPage1 = (
-  <nav>
-    <div>
-      //<img src="React-icon.svg" width="50%"></img>
-      <h1>Fun facts about React</h1>
-      <ul>
-        <li>Was first realised in 2013</li>
-        <li>Was created by Jordan Wakle</li>
-        <li>Is maintained by facebook</li>
-      </ul>
-    </div>
-  </nav>
+   <nav>
+      <div>
+         {/* <img src="React-icon.svg" width="50%"></img> */}
+         <h1>Fun facts about React</h1>
+         <ul>
+            <li>Was first realised in 2013</li>
+            <li>Was created by Jordan Wakle</li>
+            <li>Is maintained by facebook</li>
+         </ul>
+      </div>
+   </nav>
+);
+const reactPage2 = (
+   <nav>
+      <div>
+         {/* <img src="React-icon.svg" width="50%"></img> */}
+         <h1>Fun facts about React</h1>
+         <ul>
+            <li>Was first realised in 2013</li>
+            <li>Was created by Jordan Wakle</li>
+            <li>Is maintained by facebook</li>
+         </ul>
+      </div>
+   </nav>
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(nvvv());
