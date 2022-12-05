0cbe5b39b5f84129979c62e470fb14864d84883a
Musa Doci <docimusa@gmail.com>
2022-12-05 22:55:53 +0100

    test react.render

---
+++ app.js
@@ -0,0 +1,18 @@
+function nvvv() {
+    return{
+        reactPage1
+    }
+}
+const reactPage1 ()=> {(<nav>
+    <div>
+    <img src="React-icon.svg" width="50%"></img>
+    <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first realised in 2013</li>
+            <li>Was created by Jordan Wakle</li>
+            <li>Is maintained by facebook</li>
+        </ul>
+    </div>
+</nav>)}
+
+ReactDOM.createRoot(document.getElementById('root')).render(nvvv)
