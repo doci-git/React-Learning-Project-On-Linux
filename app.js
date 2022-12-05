127ae36904b167b3770557cc6fe38e58c2a5538c
Musa Doci <docimusa@gmail.com>
2022-12-05 23:04:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 function nvvv() {
-    return{
-        reactPage1
-    }
+  return reactPage1;
 }
-const reactPage1 ()=> {(<nav>
+const reactPage1 = (
+  <nav>
     <div>
-    <img src="React-icon.svg" width="50%"></img>
-    <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first realised in 2013</li>
-            <li>Was created by Jordan Wakle</li>
-            <li>Is maintained by facebook</li>
-        </ul>
+      //<img src="React-icon.svg" width="50%"></img>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first realised in 2013</li>
+        <li>Was created by Jordan Wakle</li>
+        <li>Is maintained by facebook</li>
+      </ul>
     </div>
-</nav>)}
+  </nav>
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(nvvv)
+ReactDOM.createRoot(document.getElementById("root")).render(nvvv());
