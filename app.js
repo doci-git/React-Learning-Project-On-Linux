7f633d7c810cacb3dad3ebb8b99283f43528f848
Musa Doci <docimusa@gmail.com>
2022-12-07 17:36:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
-function Nvvv() {
-   return <ReactPage1 />;
+function Tests() {
+   return (
+      <div>
+         <ReactPage1/>
+      </div>
+   );
 }
-const ReactPage1 = () => {
-   return;
+const ReactPage1 = () =>{
+   return(
+      <nav>
+         <div>
+            {/* <img src="React-icon.svg" width="50%"></img> */}
+            <h1>Fun facts about React</h1>
+            <ul>
+               <li>Was first realised in 2013</li>
+               <li>Was created by Jordan Wakle</li>
+               <li>Is maintained by facebook</li>
+            </ul>
+         </div>
+      </nav>)
+   
+}
 
-   <nav>
-      <div>
-         {/* <img src="React-icon.svg" width="50%"></img> */}
-         <h1>Fun facts about React</h1>
-         <ul>
-            <li>Was first realised in 2013</li>
-            <li>Was created by Jordan Wakle</li>
-            <li>Is maintained by facebook</li>
-         </ul>
-      </div>
-   </nav>;
-};
-const ReactPage2 = () => {
-   return;
-   <nav>
-      <div>
-         {/* <img src="React-icon.svg" width="50%"></img> */}
-         <h1>Fun facts about React</h1>
-         <ul>
-            <li>Was first realised in 2013</li>
-            <li>Was created by Jordan Wakle</li>
-            <li>Is maintained by facebook</li>
-         </ul>
-      </div>
-   </nav>;
-};
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Nvvv />);
+// const ReactPage2 = () => {
+//    return;
+//    <nav>
+//       <div>
+//          {/* <img src="React-icon.svg" width="50%"></img> */}
+//          <h1>Fun facts about React</h1>
+//          <ul>
+//             <li>Was first realised in 2013</li>
+//             <li>Was created by Jordan Wakle</li>
+//             <li>Is maintained by facebook</li>
+//          </ul>
+//       </div>
+//    </nav>;
+// };
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Tests/>);
+//ReactDOM.render(<Test />, document.getElementById("root"));
