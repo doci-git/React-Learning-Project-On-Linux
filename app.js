a11a80d370cedd735a052bcffa60f7b343693175
Musa Doci <docimusa@gmail.com>
2022-12-07 17:40:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 function Tests() {
    return (
       <div>
-         <ReactPage1/>
+         <ReactPage1 />
+         <ReactPage1 />
+         <ReactPage2 />
       </div>
    );
 }
-const ReactPage1 = () =>{
-   return(
+const ReactPage1 = () => {
+   return (
       <nav>
          <div>
-            {/* <img src="React-icon.svg" width="50%"></img> */}
+            <img src="React-icon.svg" width="20%"></img>
             <h1>Fun facts about React</h1>
             <ul>
                <li>Was first realised in 2013</li>
@@ -17,25 +19,25 @@ const ReactPage1 = () =>{
                <li>Is maintained by facebook</li>
             </ul>
          </div>
-      </nav>)
-   
-}
+      </nav>
+   );
+};
 
+const ReactPage2 = () => {
+   return (
+      <nav>
+         <div>
+            <img src="React-icon.svg" width="50%"></img>
+            <h1>Fun facts 2 about React</h1>
+            <ul>
+               <li>Was first realised in 2013</li>
+               <li>Was created by Jordan Wakle</li>
+               <li>Is maintained by facebook</li>
+            </ul>
+         </div>
+      </nav>
+   );
+};
 
-// const ReactPage2 = () => {
-//    return;
-//    <nav>
-//       <div>
-//          {/* <img src="React-icon.svg" width="50%"></img> */}
-//          <h1>Fun facts about React</h1>
-//          <ul>
-//             <li>Was first realised in 2013</li>
-//             <li>Was created by Jordan Wakle</li>
-//             <li>Is maintained by facebook</li>
-//          </ul>
-//       </div>
-//    </nav>;
-// };
-
-ReactDOM.createRoot(document.getElementById("root")).render(<Tests/>);
+ReactDOM.createRoot(document.getElementById("root")).render(<Tests />);
 //ReactDOM.render(<Test />, document.getElementById("root"));
