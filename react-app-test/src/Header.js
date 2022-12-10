28e33935b2f66d44ef97992deed467998aae0a1f
Musa Doci <docimusa@gmail.com>
2022-12-10 17:51:31 +0100

    cerate js file

---
+++ react-app-test/src/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+const Header = () => {
+    return (
+     <>
+       <header>
+          <nav>
+             <ul>
+                <li>home</li>
+                <li>contact</li>
+                <li>about</li>
+             </ul>
+          </nav>
+       </header>
+       <h1>React APP</h1>
+       <img src='/logo192.png'></img>
+       </>
+    );
+ };
+ export default Header
--- react-app-test/src/index.js
+++ react-app-test/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Header from "Header.js";
 
 function Display() {
   return(
@@ -11,23 +12,7 @@ function Display() {
   )
 }
 
-const Header = () => {
-   return (
-    <>
-      <header>
-         <nav>
-            <ul>
-               <li>home</li>
-               <li>contact</li>
-               <li>about</li>
-            </ul>
-         </nav>
-      </header>
-      <h1>React APP</h1>
-      <img src='/logo192.png'></img>
-      </>
-   );
-};
+
 const Main = () => {
    return (
       <div>
