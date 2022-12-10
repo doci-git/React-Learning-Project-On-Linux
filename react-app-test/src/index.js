04f5ed4410b0b22b8d42fdbe722611804bba0e35
Musa Doci <docimusa@gmail.com>
2022-12-10 17:16:02 +0100

    Update index.js

--- react-app-test/src/index.js
+++ react-app-test/src/index.js
@@ -1,3 +1,8 @@
-import React from 'react';
-import ReactDOM  from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
 
+function Header() {
+   return <h1>start start strat</h1>;
+  }
+  ReactDOM.render(<Header />,document.getElementById("root"));
+  
