1aef3ef6a4fd9e49f89a67ea90ac7573776a06f2
Musa Doci <docimusa@gmail.com>
2022-12-10 17:38:21 +0100

    Update index.js

--- react-app-test/src/index.js
+++ react-app-test/src/index.js
@@ -1,7 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function Header() {
-   return <h1>start start strat</h1>;
+function Display() {
+  return(
+    <>
+    <Header/>
+    <Main/>
+    <Footer/>
+    </>
+  )
 }
-ReactDOM.createRoot(document.getElementById("root")).render(<Header />);
+
+const Header = () => {
+   return (
+    <>
+      <header>
+         <nav>
+            <ul>
+               <li>home</li>
+               <li>contact</li>
+               <li>about</li>
+            </ul>
+         </nav>
+      </header>
+      <img src='/logo192.png'></img>
+      </>
+   );
+};
+const Main = () => {
+   return (
+      <div>
+         <h2>main app</h2>
+         <p>hello this is thet start main app </p>
+      </div>
+   );
+};
+const Footer = () => {
+   return <footer> all rights resreved </footer>;
+};
+ReactDOM.createRoot(document.getElementById("root")).render(<Display />);
