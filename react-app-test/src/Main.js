1ac7f31a3dc3dd3a5326c0a0f6dc7249b29f8b9d
Musa Doci <docimusa@gmail.com>
2022-12-11 13:43:19 +0100

    Update Main.js

--- react-app-test/src/Main.js
+++ react-app-test/src/Main.js
@@ -1,18 +1,17 @@
 import React from "react";
 
-
 const Main = () => {
-    return (
-       <div className="main-div">
-       <div className="img-sfondo"></div>
-          <h2>main app</h2>
-          <p>hello this is thet start main app </p>
-          <ul>
+   return (
+      <div className="main-div">
+         <div className="img-sfondo"></div>
+         <h2>main app</h2>
+         <ul>
+            <li>hello this is thet start main app </li>
             <li>firts realise was 2013</li>
             <li>col to work</li>
             <li>is maintained by facebook</li>
-          </ul>
-       </div>
-    );
- };
- export default Main
+         </ul>
+      </div>
+   );
+};
+export default Main;
