84bdd8c83bc0b8782ebd178189fc527445824431
Musa Doci <docimusa@gmail.com>
2022-12-11 15:02:17 +0100

    refacory

--- react-app-test/src/index.js
+++ react-app-test/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./Header.js";
-import Main from "./Main.js"
-import Footer from "./Footer.js";
+import Header from "./components/Header.js";
+import Main from "./components/Main.js"
+import Footer from "./components/Footer.js";
 import "./App.css"
 
 function Display() {
