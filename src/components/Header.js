0861f09df1b877ca90959bb8f30bcc3d7a6701ac
Musa Doci <docimusa@gmail.com>
2023-01-05 23:39:28 +0100

    foto

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,9 @@ import React from "react";
 const Header = () => {
    return(
     <div className="header-container">
-        <img src="" alt="" />
-        <h2>Cuck Noris Joke</h2>
+        <img src="./images/meme2.jpg" alt="" />
+        <h2>Meme Generator</h2>
+        <h4>project 2</h4>
     </div>
    )
 };
