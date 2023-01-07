db94f17d60aec3c193e34c1138d7bc30d5090ee3
Musa Doci <docimusa@gmail.com>
2023-01-07 22:15:05 +0100

    click handler

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,15 +1,20 @@
 import React from "react";
 const Form = () => {
+   function generate() {
+      console.log("good job");
+   }
    return (
-      <div>
-         <form action="">
+      <div className="div-form">
+         <form className="form" action="">
             <input type="text" placeholder="Top text" className="form-input" />
             <input
                type="text"
                placeholder="Bottom text"
                className="form-input"
             />
-            <button className="form-button">generate meme</button>
+            <button className="form-button" onClick={generate}>
+               Generate meme
+            </button>
          </form>
       </div>
    );
