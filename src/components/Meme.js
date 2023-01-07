073638897932985b676e8afa39dcff3a9f520b17
Musa Doci <docimusa@gmail.com>
2023-01-07 23:05:23 +0100

    getting data

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,21 +1,19 @@
 import React from "react";
+import memeData from "./memeData";
 const Form = () => {
    function generate() {
-      console.log("good job");
+      const memesArray = memeData.data.memes;
+      const random = Math.floor(Math.random() * memesArray.length);
+      const url = memesArray[random].url;
+      console.log(url);
    }
    return (
       <div className="div-form">
-         <form className="form" action="">
-            <input type="text" placeholder="Top text" className="form-input" />
-            <input
-               type="text"
-               placeholder="Bottom text"
-               className="form-input"
-            />
-            <button className="form-button" onMouseEnter={generate}>
-               Generate meme
-            </button>
-         </form>
+         <input type="text" placeholder="Top text" className="form-input" />
+         <input type="text" placeholder="Bottom text" className="form-input" />
+         <button className="form-button" onClick={generate}>
+            Generate meme
+         </button>
       </div>
    );
 };
