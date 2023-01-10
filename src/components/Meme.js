094f41c8d5ed2814eadfdc3357e8d166a6c6269c
Musa Doci <docimusa@gmail.com>
2023-01-10 19:15:29 +0100

    update

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -14,7 +14,7 @@ const Form = () => {
          <button className="form-button" onClick={generate}>
             Generate meme
          </button>
-         <img src="" alt="" />;
+         <img src=" " alt="" />
       </div>
    );
 };
