2f46270660bd9ba828398708795a4f9abd29dced
Musa Doci <docimusa@gmail.com>
2023-03-05 15:03:25 +0100

    tetst nav with bnooostrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header.js";
 import Form from "./components/Meme.js";
 import Main from "./components/Main.js";
+import NavTest from "./components/Footer.js";
 // import Footer from "./components/Footer.js";
 
 function Display() {
@@ -10,6 +11,7 @@ function Display() {
          <Header />
          <Form />
          <Main />
+         <NavTest />
       </>
    );
 }
