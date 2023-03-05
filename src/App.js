ae5051b0349a5b49651d3350a37c20854f350455
Musa Doci <docimusa@gmail.com>
2023-03-05 18:56:04 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
 import Header from "./components/Header.js";
 import Form from "./components/Meme.js";
 import Main from "./components/Main.js";
-import NavTest from "./components/Footer.js";
+import Footer from "./components/Footer.js";
 // import Footer from "./components/Footer.js";
 
 function Display() {
@@ -11,7 +14,7 @@ function Display() {
          <Header />
          <Form />
          <Main />
-         <NavTest />
+         <Footer />
       </>
    );
 }
