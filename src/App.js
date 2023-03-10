d9ea47214c2faec8fae4c4484f3858c561c84a90
Musa Doci <docimusa@gmail.com>
2023-03-10 18:27:47 +0100

    import nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { ReactDOM } from "react";
+import React from "react";
 import "./App.css";
+import Nav from "./components/Nav";
 
 function App() {
-   return <div className="App"></div>;
+   return (
+      <div className="App">
+         <Nav />
+      </div>
+   );
 }
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,7 @@
+import { ReactDOM } from "react";
+import React from "react";
+
+const Nav = () => {
+   return <h2>hello</h2>;
+};
+export default Nav;
