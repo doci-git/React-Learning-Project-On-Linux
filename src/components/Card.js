4c363ba1eb13204482f9bc2c3075e1d1cda43751
Musa Doci <docimusa@gmail.com>
2023-03-10 21:32:04 +0100

    card

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,15 @@ import React from "react";
 
 const Card = (props) => {
    return (
-      <div>
-         <h1>{props.item.title}</h1>
-         <p>{props.item.description}</p>
+      <div className="div-card">
+         <section>
+            <div className="picture">
+               <img src={props.item.coverImg} alt="" />
+            </div>
+            <h1 className="card-title">{props.item.title}</h1>
+            <p className="card-content">{props.item.description}</p>
+            <button>Read</button>
+         </section>
       </div>
    );
 };
--- src/components/data.js
+++ src/components/data.js
@@ -4,11 +4,18 @@ export default [
       title: "Learn Wedding Photography",
       description:
          "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
-      coverImg: "wedding-photography.png",
-      stats: {
-         rating: 5.0,
-         reviewCount: 30,
-      },
+      coverImg:
+         "https://static01.nyt.com/images/2023/03/10/multimedia/10saudi-iran-mqlb/10saudi-iran-mqlb-threeByTwoSmallAt2X.jpg?format=pjpg&quality=75&auto=webp&disable=upscale",
+      location: "Online",
+      date: 27,
+   },
+   {
+      id: 2,
+      title: "Learn Wedding Photography",
+      description:
+         "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+      coverImg:
+         "https://static01.nyt.com/images/2023/03/10/multimedia/10saudi-iran-mqlb/10saudi-iran-mqlb-threeByTwoSmallAt2X.jpg?format=pjpg&quality=75&auto=webp&disable=upscale",
       location: "Online",
       date: 27,
    },
