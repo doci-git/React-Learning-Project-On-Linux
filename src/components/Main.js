6c93dc908fc0a5f298d632024a3af12f421de383
Musa Doci <docimusa@gmail.com>
2023-03-13 20:29:47 +0100

    create a form

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Main = () => {
+   return (
+      <main>
+         <form>
+            <input type="text" />
+            <input type="text" />
+            <button>Get a New Meme Image</button>
+         </form>
+      </main>
+   );
+};
+export default Main;
