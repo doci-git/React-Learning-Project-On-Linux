0c9806f276b67acae7c3521e99ad7ab7b2f0fb6f
Musa Doci <docimusa@gmail.com>
2023-03-13 19:41:02 +0100

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import React from "react";
 function Header() {
    return (
       <header className="header-container">
-         <img src="./images/meme-logo.png" alt="" />
+         <img src="./meme-logo.png" alt="" />
          <h2>Meme Generator</h2>
          <h4>React project 2</h4>
       </header>
