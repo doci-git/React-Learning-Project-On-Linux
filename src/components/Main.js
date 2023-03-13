1267ce64b7246572091511a91402d3eb4e91555a
Musa Doci <docimusa@gmail.com>
2023-03-13 20:37:34 +0100

    style

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { useState } from "react";
 
 const Main = () => {
    return (
       <main>
-         <form>
-            <input type="text" />
-            <input type="text" />
-            <button>Get a New Meme Image</button>
+         <form className="div-form">
+            <input className="form-input" type="text" />
+            <input className="form-input" type="text" />
+            <button className="form-button">Get a New Meme Image</button>
          </form>
       </main>
    );
