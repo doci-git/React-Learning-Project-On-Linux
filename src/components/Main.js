6547d58b1e69a5e39ac6081742fceb0877d6618c
Musa Doci <docimusa@gmail.com>
2023-03-21 20:09:18 +0100

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,11 +2,11 @@ import React from "react";
 import data from "./data";
 
 const Main = () => {
+   const [img, setImg] = React.useState("");
    const butt = () => {
       const item = data.data.memes;
       const random = Math.floor(Math.random(item) * item.length);
-      const url = item[random].url;
-      console.log(url);
+      setImg(item[random].url);
    };
    return (
       <main>
@@ -17,6 +17,7 @@ const Main = () => {
                Get a New Meme Image
             </button>
          </div>
+         <img src={img} className="img-meme" alt="" />
       </main>
    );
 };
